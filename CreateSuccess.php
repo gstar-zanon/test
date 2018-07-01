da766304fc56b8967378cee4d1bf971ba61c23a9
レヘルン <gstar-zanon@users.noreply.github.com>
2018-07-01 23:12:50 +0900

    タイトル入力

--- CreateSuccess.php
+++ CreateSuccess.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-	<title>テストサイト</title>
+	<title>ユーザー作成完了</title>
 	<meta http-equiv="Content-type" content="text/html; charaset=UTF-8">
 	<link rel="stylesheet" type="text/css" href="../CSS/main.css">
 	<?php require_once __DIR__.'/ClassList/Common.php' ?>
